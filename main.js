6c8d6703681b0cca220f658a52fecee87b1ac422
AftDawn <Declanjeffrey6@gmail.com>
2022-05-03 07:22:26 +1000

    added commands

--- main.js
+++ main.js
@@ -10,5 +10,21 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		interaction.reply('pong');
+	}
+	else if (commandName === 'server') {
+		interaction.reply(`Server name: ${interaction.guild.name}\nServer ID: ${interaction.guild.id}\nServer owner: ${interaction.guild.owner.user.tag}\nServer count: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		interaction.reply(`User name: ${interaction.user.tag}\nUser ID: ${interaction.user.id}`);
+	}
+});
+
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
